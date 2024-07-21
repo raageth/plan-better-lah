e3b1341a8776b198fec9ee96d45ae56101e07288
raageth <chaygareth@gmail.com>
2024-07-21 09:57:37 +0800

    Add ci and change tests

--- tests/algo/test_db.py
+++ tests/algo/test_db.py
@@ -1,12 +1,1 @@
-import unittest
-from algo.mod_planner import ModPlanner
-
-class TestDB(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def test_plan(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
+import pytest
--- tests/algo/test_mod_planner.py
+++ tests/algo/test_mod_planner.py
@@ -1,39 +1,30 @@
-import unittest
+import pytest
 import json
 import os
 from algo.mod_planner import ModPlanner
 
-class TestModPlanner(unittest.TestCase):
-    def setUp(self):
-        pass
+def load_test_file(file_name):
+    with open(os.path.join(os.path.dirname(__file__), '..', 'testcases', file_name), 'r') as f:
+        return json.load(f)
 
-    def test_s2_5m_positive(self):
-        """
-        Test sem2, 5 mods, positive case
-        """
+def test_s2_5m_positive():
+    """
+    Test sem2, 5 mods, positive case
+    """
+    test_modules = ['CS1101S', 'MA1521', 'MA1522', 'IS1108', 'GEA1000']
+    test_result_url = "https://nusmods.com/timetable/sem-2/share?CS1101S=TUT:02,REC:01,LEC:1&MA1521=TUT:22,LEC:2&MA1522=TUT:15,LEC:2&IS1108=TUT:08,LEC:1&GEA1000=TUT:D28"
 
-        test_modules = ['CS1101S', 'MA1521', 'MA1522', 'IS1108', 'GEA1000']
-        with open(os.path.join(os.path.dirname(__file__), '..', 'testcases', 's2_5m_positive.json'), 'r') as f:
-            test_mod_info = json.load(f)
-        test_result_url = "https://nusmods.com/timetable/sem-2/share?CS1101S=TUT:06,REC:02,LEC:1&MA1521=TUT:16,LEC:1&MA1522=TUT:7,LEC:1&IS1108=TUT:16,LEC:1&GEA1000=TUT:D54"
-        
-        planner = ModPlanner(test_modules, test_mod_info, 2)
-        url = planner.solve()
-        self.assertEqual(url, test_result_url)
-    
-    def test_s1_8m_negative(self):
-        """
-        Test sem1, 8 mods, negative case
-        """
+    planner = ModPlanner(test_modules, load_test_file('s2_5m_positive.json'), 2, 10)
+    url = planner.solve()
+    assert url == test_result_url
 
-        test_modules = ['CS1101S', 'MA1521', 'MA1522', 'IS1108', 'GEA1000', 'CS1231', 'CS2030', 'CS2040']
-        with open(os.path.join(os.path.dirname(__file__), '..', 'testcases', 's1_8m_negative.json'), 'r') as f:
-            test_mod_info = json.load(f)
-        test_result_url = ""
-        
-        planner = ModPlanner(test_modules, test_mod_info, 1)
-        url = planner.solve()
-        self.assertEqual(url, test_result_url)
+def test_s1_8m_negative():
+    """
+    Test sem1, 8 mods, negative case
+    """
+    test_modules = ['CS1101S', 'MA1521', 'MA1522', 'IS1108', 'GEA1000', 'CS1231', 'CS2030', 'CS2040']
+    test_result_url = ""
 
-if __name__ == '__main__':
-    unittest.main()
+    planner = ModPlanner(test_modules, load_test_file('s1_8m_negative.json'), 1, 10)
+    url = planner.solve()
+    assert url == test_result_url
