09bc0ddf0deb99def7817498bd47cd66014fbebe
raageth <chaygareth@gmail.com>
2024-07-28 11:36:36 +0800

    Add tests

--- tests/algo/test_mod_planner.py
+++ tests/algo/test_mod_planner.py
@@ -13,11 +13,11 @@ def test_s2_5m_positive():
     Test sem2, 5 mods, positive case
     """
     test_modules = ['CS1101S', 'MA1521', 'MA1522', 'IS1108', 'GEA1000']
-    # test_result_url = "https://nusmods.com/timetable/sem-2/share?CS1101S=TUT:02,REC:01,LEC:1&MA1521=TUT:22,LEC:2&MA1522=TUT:15,LEC:2&IS1108=TUT:08,LEC:1&GEA1000=TUT:D28"
-
-    planner = ModPlanner(test_modules, load_test_file('s2_5m_positive.json'), 2, 10)
-    url = planner.solve()
-    # assert url == test_result_url
+    test_blocked_timings = {1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
+    test_result_url = "https://nusmods.com/timetable/sem-2/share?CS1101S=TUT:01,REC:02,LEC:1&MA1521=TUT:5,LEC:2&MA1522=TUT:18,LEC:2&IS1108=LEC:2,TUT:10&GEA1000=TUT:D25"
+    test_mod_info = load_test_file('s2_5m_positive.json')
+    planner = ModPlanner(test_modules, test_mod_info, 2, 24, test_blocked_timings, test_mod_info)
+    (url, best_info) = planner.solve()
     assert url
 
 def test_s1_8m_negative():
@@ -25,9 +25,9 @@ def test_s1_8m_negative():
     Test sem1, 8 mods, negative case
     """
     test_modules = ['CS1101S', 'MA1521', 'MA1522', 'IS1108', 'GEA1000', 'CS1231', 'CS2030', 'CS2040']
+    test_blocked_timings = {1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
     test_result_url = ""
-
-    planner = ModPlanner(test_modules, load_test_file('s1_8m_negative.json'), 1, 10)
-    url = planner.solve()
-    # assert url == test_result_url
+    test_mod_info = load_test_file('s1_8m_negative.json')
+    planner = ModPlanner(test_modules, test_mod_info, 1, 6, test_blocked_timings, test_mod_info)
+    (url, best_info) = planner.solve()
     assert not url
